2d51b0643b5fdbb69ee948bb9cdf53ce575cf9e7
rilo <rilo.raspopo37@gmail.com>
2025-11-19 10:05:23 +0700

    ver3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3165417701"
+    serviceWorkerVersion: "2403078176"
   }
 });
